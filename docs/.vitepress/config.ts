d44b3894d8dd4e1c8c69cdc420ae73af74b8999a
annanLv <1246359021@qq.com>
2023-07-07 14:19:02 +0800

    :art: 初步重构

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,48 @@
+import { defineConfig } from "vitepress";
+import { head, nav, algolia } from "./configs";
+import { getSideBarList } from "../util";
+
+export default defineConfig({
+  title: "an nanLv's blog",
+  base: "/my-blog/",
+  description: '安楠的博客',
+
+  cleanUrls: true,
+  lastUpdated: true,
+
+  head,
+
+  /* markdown 配置 */
+  markdown: {
+    // 行数
+    lineNumbers: true
+  },
+
+  themeConfig: {
+    logo: '/img.png',
+    algolia,
+    outline: {
+      level: 'deep',
+      label: '目录'
+    },
+    nav,
+    socialLinks: [{ icon: 'github', link: 'https://github.com/annanLv' }],
+
+    darkModeSwitchLabel: '外观',
+    returnToTopLabel: '返回顶部',
+    lastUpdatedText: '上次更新',
+    sidebar: {
+      ...getSideBarList('vue'),
+      ...getSideBarList('blog')
+    },
+    footer: {
+      message: '如有转载或 CV 的请标注本站原文地址',
+      copyright: 'Copyright © 2019-present annanLv'
+    },
+    docFooter: {
+      prev: '上一篇',
+      next: '下一篇'
+    },
+  }
+})
+
